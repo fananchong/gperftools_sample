883d7540888034aca1f2b4b9bf33e759e0aaaee9
fananchong <fananchong@126.com>
2018-01-24 16:24:32 +0800

    add submodules

---
+++ deps/_packages/_load_env.bat
@@ -0,0 +1,16 @@
+set CUR_PATH=%~dp0
+
+REM 设置VS路径
+set _devenv="devenv.com"
+
+REM 设置curl路径
+set _curl=%CUR_PATH%\..\..\tools\build\curl-win\curl.exe
+
+REM 设置7z路径
+set _7z=%CUR_PATH%\..\..\tools\build\7z-win\7z.exe
+
+REM 设置tar路径
+set _tar=%CUR_PATH%\..\..\tools\build\tar-win\WinTar_1_0_2.exe
+
+REM 设置sed路径
+set _sed=%CUR_PATH%\..\..\tools\build\sed-win\sed.exe
---
+++ deps/_packages/docker-build/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -ex
+
+out_path=$PWD/../../
+
+docker build -t plengine/build-centos7 ./centos7.3
+
+docker build -t plengine/build-boost ./boost
+docker run --rm -v $out_path:/tmp/home plengine/build-boost /bin/bash "/setup.sh"
+
