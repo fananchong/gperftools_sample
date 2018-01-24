006556018b347e6415cb439ab59c98ee63ff7c97
fananchong <fananchong@126.com>
2018-01-24 16:52:00 +0800

    build in linux

--- deps/_packages/docker-build/build.sh
+++ deps/_packages/docker-build/build.sh
@@ -6,6 +6,6 @@ out_path=$PWD/../../
 
 docker build -t plengine/build-centos7 ./centos7.3
 
-docker build -t plengine/build-boost ./boost
-docker run --rm -v $out_path:/tmp/home plengine/build-boost /bin/bash "/setup.sh"
+docker build -t plengine/build-gperftools ./gperftools
+docker run --rm -v $out_path:/tmp/home plengine/build-gperftools /bin/bash "/setup.sh"
 
